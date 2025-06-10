5ea7c4c5aa214da82b9e3bbb23df1d98ce1b5373
Nadikafiqri <nadikafiqri09@gmail.com>
2025-06-10 10:45:21 +0700

    Delete MODUL/Modul 6/Modul6 directory

--- MODUL/Modul 6/Modul6/src/main/java/main/modul6/Actions/MhsAction.java
+++
@@ -1,6 +0,0 @@
-package main.modul6.Actions;
-
-public interface MhsAction {
-    void ReportItem();
-    void ViewReportItem();
-}
--- MODUL/Modul 6/Modul6/src/main/java/main/modul6/Model/ExceptionCustom.java
+++
@@ -1,8 +0,0 @@
-package main.modul6.Model;
-
-public class ExceptionCustom extends Exception{
-    
-    public ExceptionCustom(String Message){
-        super(Message);
-    }
-}
